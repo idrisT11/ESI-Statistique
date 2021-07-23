89b657f7a5296aa81e5e4cbfef25f62d557e474c
idrisT11 <64688223+idrisT11@users.noreply.github.com>
2021-07-23 19:33:53 +0100

    Update utils.js

--- Web/utils.js
+++ Web/utils.js
@@ -1,8 +1,20 @@
 function getEffectiveMax(a) 
 {
-    if (a % 100 == 0) 
+    if (a < 6000) //temporaire
+    {
+        if (a % 10 == 0) 
 
-        return a;
-    else 
-        return a - (a % 100) + 100;
+            return a;
+        else 
+            return a - (a % 10) + 10;
+    }
+    else
+    {
+        if (a % 100 == 0) 
+
+            return a;
+        else 
+            return a - (a % 100) + 100;
+    }
+
 }
